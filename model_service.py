702ab8a3063c5a72fdcbc0b196530d53a92458fa
Sergiy Yemelyanov <yemelyanov1994@gmail.com>
2023-11-15 19:03:35 +0200

    Added improvements to handle multiple outputs.

--- model_service.py
+++ model_service.py
@@ -4,57 +4,87 @@ import tensorflow as tf
 
 
 def train_model(data, job_token):
-    epochs = data["configuration"]["epochs"]
-    optimizer = data["configuration"]["optimizer"]
-    activation = data["configuration"]["activation"]
-    loss = data["configuration"]["loss"]
-    layers = data["configuration"]["layers"]
-    batch_size = data["configuration"]["batch_size"]
-    model_id = data["processStructureId"]
+    try:
+        print(f"Incoming job token: {job_token}")
 
-    models_dir = "models"
-    model_path = f"{models_dir}/{model_id}.h5"
+        # network parameters
+        epochs = data["configuration"]["epochs"]
+        optimizer = data["configuration"]["optimizer"]
+        activation = data["configuration"]["activation"]
+        loss = data["configuration"]["loss"]
+        layers = data["configuration"]["layers"]
+        batch_size = data["configuration"]["batch_size"]
+        model_id = data["processStructureId"]
 
-    if not os.path.isdir(models_dir):
-        os.makedirs(models_dir)
+        # directories and model path
+        models_dir = "models"
+        model_path = f"{models_dir}/{model_id}.h5"
 
-    train_x = tf.convert_to_tensor(
-        [input_data["data"] for input_data in data["training"]["inputs"]],
-        dtype=tf.float32,
-    )
+        # creating model directory if not exists
+        if not os.path.isdir(models_dir):
+            os.makedirs(models_dir)
 
-    train_y = tf.convert_to_tensor(
-        data["training"]["outputs"][0]["data"], dtype=tf.float32
-    )
-
-    test_x = tf.convert_to_tensor(
-        [input_data["data"] for input_data in data["checking"]["inputs"]],
-        dtype=tf.float32,
-    )
-
-    test_y = tf.convert_to_tensor(
-        data["checking"]["outputs"][0]["data"], dtype=tf.float32
-    )
-
-    model = tf.keras.models.Sequential()
-
-    for _ in range(layers):
-        model.add(
-            tf.keras.layers.Dense(
-                4, input_shape=(train_x.shape[1],), activation=activation
-            )
+        # training data
+        train_x = tf.convert_to_tensor(
+            [input_data["data"] for input_data in data["training"]["inputs"]],
+            dtype=tf.float32,
         )
-    model.add(tf.keras.layers.Dense(1, activation="linear"))
+        train_y_list = []
+        for output in data["training"]["outputs"]:
+            train_y_list.append(
+                tf.convert_to_tensor(
+                    output["data"],
+                    dtype=tf.float32,
+                )
+            )
 
-    model.compile(optimizer=optimizer, loss=loss, metrics=["mse"])
-    model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=1)
+        # evaluating data
+        test_x = tf.convert_to_tensor(
+            [input_data["data"] for input_data in data["checking"]["inputs"]],
+            dtype=tf.float32,
+        )
+        test_y_list = []
+        for output in data["checking"]["outputs"]:
+            test_y_list.append(
+                tf.convert_to_tensor(
+                    output["data"],
+                    dtype=tf.float32,
+                )
+            )
 
-    loss, accuracy = model.evaluate(test_x, test_y)
+        # init model
+        model = tf.keras.models.Sequential()
 
-    model.save(filepath=model_path)
+        # add layers
+        for _ in range(layers):
+            model.add(
+                tf.keras.layers.Dense(
+                    4, input_shape=(train_x.shape[1],), activation=activation
+                )
+            )
 
-    print(f"Loss on test data: {loss}")
-    print(f"Accuracy on test data: {accuracy}")
+        # for approximation purposes we always need a layer with linear activation function
+        model.add(tf.keras.layers.Dense(4, activation="linear"))
+
+        # model compilation
+        model.compile(optimizer=optimizer, loss=loss, metrics=["mse"])
+
+        # model training
+        model.fit(
+            train_x, train_y_list, epochs=epochs, batch_size=batch_size, verbose=1
+        )
+
+        # evaluating model on test data
+        loss, accuracy = model.evaluate(test_x, test_y_list)
+
+        # saving model
+        model.save(filepath=model_path)
+
+        print(f"Loss on test data: {loss}")
+        print(f"Accuracy on test data: {accuracy}")
+        print(f"Add job to responses queue that indicate that job is finished.")
+    except:
+        print("An exception occurred")
 
 
 def model_predict(model, prediction_data):
